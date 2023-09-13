6df7c949fb6531d581ed554ff9fbed88812ef109
Iñigo Telleria <inigo@arima.eu>
2023-09-13 17:39:48 +0200

    Initial import

---
+++ src/main/java/eu/arima/demo/DemoController.java
@@ -0,0 +1,14 @@
+package eu.arima.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/hello")
+    String hello() {
+        return "Hello!";
+    }
+
+}
---
+++ src/test/java/eu/arima/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.arima.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

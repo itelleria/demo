7d43bcff20e641afb0d21398106aaf031692a070
Iñigo Telleria <inigo@arima.eu>
2023-10-16 17:50:13 +0200

    Return current datetime

--- src/main/java/eu/arima/demo/DemoController.java
+++ src/main/java/eu/arima/demo/DemoController.java
@@ -3,12 +3,14 @@ package eu.arima.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 public class DemoController {
 
-    @GetMapping("/hello")
+    @GetMapping("/time")
     String hello() {
-        return "Hello!";
+        return "Current time: " + LocalDateTime.now();
     }
 
 }

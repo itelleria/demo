244e02b0bacc4affe319a21ae2c5252b4c4dcbbb
Iñigo Telleria <inigo@arima.eu>
2023-10-18 11:02:03 +0200

    Add database endpoint

--- src/main/java/eu/arima/demo/DemoController.java
+++ src/main/java/eu/arima/demo/DemoController.java
@@ -1,5 +1,6 @@
 package eu.arima.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,28 @@ import java.time.LocalDateTime;
 @RestController
 public class DemoController {
 
+    private final String databaseUrl;
+
+    private final String databaseUsername;
+
+    public DemoController(@Value("${database-url:not-found}") String databaseUrl, @Value("${database-username:not-found}")String databaseUsername) {
+        this.databaseUrl = databaseUrl;
+        this.databaseUsername = databaseUsername;
+    }
+
+    @GetMapping("/version")
+    String version() {
+        return "v4.0";
+    }
+
     @GetMapping("/time")
     String hello() {
         return "Current time: " + LocalDateTime.now();
+    }
+
+    @GetMapping("/database")
+    String database() {
+        return this.databaseUrl + ":" + this.databaseUsername;
     }
 
 }
